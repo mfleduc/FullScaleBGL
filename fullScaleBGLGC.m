68a8534d1d3885e2066988554704e89d69459e2e
Matthew Leduc <male7736@frodo.int.colorado.edu>
2025-04-21 15:53:45 -0600

    Updating run scripts

--- fullScaleBGLGC.m
+++ fullScaleBGLGC.m
@@ -1,90 +1,52 @@
-function fullScaleBGLGC(lambdas,lvls)
+function fullScaleBGLGC(input)
 %% Full scale basis graphical lasso
 % clear variables; close all;
 fprintf('Running Full-Scale Basis Graphical Lasso with %d levels of resolution and lambdas %.4f to %.4f\n'...
-    ,lvls,lambdas(1),lambdas(end))
-nneeds = [12,60,252,1020]+1;
-rng(6516156)
-cd('/glade/work/mleduc/GP emulation/needlet fbgl');
-addpath( genpath('/glade/work/mleduc/Ionosphere_inversion/NeedMat/') );
-addpath( genpath('/glade/work/mleduc/Ionosphere_inversion/Spherical-Harmonic-Transform/') );
-addpath( genpath('/glade/work/mleduc/Ionosphere_inversion/spherepts/') );
-addpath( genpath('/glade/work/mleduc/BigQUIC_release/'))
-addpath( genpath('/glade/work/mleduc/MultivariateBasisGraphicalLasso'))
-addpath( genpath('/glade/work/mleduc/FMGL-0/'))
-addpath( genpath('/glade/work/mleduc/GP emulation/needlet fbgl/NeedletBGL'));
-addpath( genpath('/glade/work/mleduc/S2-Sampling-Toolbox'));
-
-load(sprintf('needlets degree resolution j=5.mat'), 'A');
-load('mean of beta algorithm residual transform alt final.mat' , ...
-     'testdata','LAT', 'LON' );
-% lambdas = [1,0.5,0.25,0.1,0.05,0.03,0.025];
-%% Split into time steps
-timendx = 1;
-testdata = reshape(testdata, [],25,6); 
-data = testdata - mean(testdata, 2);
-data = data(:,:,timendx)./std(data(:,:,timendx),[],2);
-%% 
+    ,input.lvls,input.lambda(1),input.lambda(end))
+fprintf('*******GASPARI-COHN COVARIANCE********\n');
+%% Example optimization parameters
 optparams = struct; 
 optparams.algorithm='simanneal';
-% optparams.ranges = [[0.001,1];... %alpha
-%     [0.001,1];...  % 0.5*chordal decorrelation distance (c parameter)
-%     [-9,-1];... %log10(nugget)
-%     [0.001,20];...% Wendland decorrelation distance]
-%     [0.001,1]]; %alpha for Wendland
-% optparams.ranges = [-10,1];
-optparams.ranges = [[0.001,1];[.0001,0.5]];
-optparams.maxiters = 500; %number of simulated annealing steps
+optparams.ranges = [[-3,3];[0.001,1500];[.0001,0.5]];
+optparams.maxiters = 5000; %number of simulated annealing steps
 optparams.npts = 501; %Number of lattice points
-optparams.x0 = [0.25,0.07];%Initial guess
+optparams.x0 = [0,750,0.25];%Initial guess
 optparams.pchange = 0.9;
 %%
-A = [ones(64800,1)/2/sqrt(pi), A]; 
-ncvs = 5;
-nptseach = 8000;
-np = nan(ncvs, size(optparams.ranges,1)+1);
-ndcs = randperm(size(A,1));
-covmodel = cell(1,ncvs);
-for nn = 1:ncvs
-        fprintf('********************Fitting residual model, Training Set %d **********************\n',nn);
-        thesendcs = ndcs((nn-1)*nptseach+1:nn*nptseach);
-        Ahat = A(thesendcs,1:nneeds(lvls));
-        thisdata = data(thesendcs,:);
-        % [~,tausq] = FitNugget(Ahat,thisdata);
-        LATd=LAT(thesendcs);LONd=LON(thesendcs);
-        [spx,spy,spz] = sph2cart(pi/180*LONd, pi/180*LATd,1);
-        spc=[spx,spy,spz];
-        d = real(acosd(spc*spc')) ; %d = d - diag(diag(d));
- %imaginary values on the diagonal sometimes        
-        d = 2*sind(d/2); %chordal distance required for gaspari cohn correlation fn
-        covmodel{nn} = @(x)gentemp([x(1:2),-0.1],d);%+gentemp([x(3:4),-0.1],d);
-        % covmodel = @(x)gentemp([x(1:2),-0.1], 2*sind(d/2)) + WendlandCov(x(3:end), d, 'cov');
-        
-        fprintf('Fitting residual parameters...');
-        [val,obj,np(nn,:)] = CalculateInitialGuess(covmodel{nn}, Ahat,thisdata, optparams);
-        fprintf('Done\n');
-end
+fprintf('********************Fitting residual model**********************\n');
+
+[spx,spy,spz] = sph2cart(pi/180*input.LON(:), pi/180*input.LAT(:),1);
+spc=[spx,spy,spz];
+d = real(acosd(spc*spc')) ; %d = d - diag(diag(d));
+%imaginary values on the diagonal sometimes        
+d = 2*sind(d/2); %chordal distance required for gaspari cohn correlation fn
+covmodel = @(x)gentemp([x(2:3),-0.1],d,x(1));%+gentemp([x(3:4),-0.1],d);
+% covmodel = @(x)gentemp([x(1:2),-0.1], 2*sind(d/2)) + WendlandCov(x(3:end), d, 'cov');
+[val,obj,np ] = CalculateInitialGuess(covmodel{1}, A ,testdata, optparams);
+fprintf('Done\n');
+alpha=np(1);
 fnname = func2str(covmodel{1});
 fnname = fnname(5:end);
 ndx = strfind(fnname, '('); 
 fnname = fnname(1:ndx(1)-1);
+disp(toc)
 for l1 = 1:length(lambdas) %% cross-validate for each lambda
     lambda = lambdas(l1);
-    Qest = cell(ncvs,1);
+    Qest = cell(1,1);
     % np = [];
-    fprintf('lambda = %.6f, calculating Q|Sigma\n');
+    fprintf('lambda = %.6f, calculating Q|Sigma\n',lambda);
     %% We want to fit a Wendland covariance
-    for nn = 1:ncvs
-        fprintf('***************Training set %d***************\n');
-        thesendcs = ndcs((nn-1)*nptseach+1:nn*nptseach);
-        Ahat = A(thesendcs,1:nneeds(lvls));    
-        thisdata = data(thesendcs,:);
-        [Qest{nn}] = CalculateQ(thisdata, Ahat, lambda, ...
-            1e-2,covmodel{nn},np(nn,2:end), strcmpi(fnname,'MarkovRandomField') );
-    end
     
-         save(sprintf('FSBGL Nugget first/GC/lower tolerance/FSBGL results %d levels time %d lambda %.4f covmodel gentemp.mat',lvls,timendx,lambda ),...
-        'Qest', 'np', 'optparams', 'timendx','covmodel','lambda','ndcs','nptseach','-v7.3')
+    [Qest{1}] = CalculateQ(testdata, A, lambda, ...
+        2e-2,covmodel{1},np, strcmpi(fnname,'MarkovRandomField') );
+    likelihood = NoiseObjective( np(2:end), covmodel{1},...
+        Qest{1},A,testdata, strcmpi(fnname,'MarkovRandomField'));
+    svdir = 'FSBGL results/temperature/GC/';
+    mkdir(svdir);
+     save(sprintf([svdir, ...
+         'FSBGL results %d levels lambda %.4f covmodel %s.mat'],lvls,lambda,fnname ),...
+    'Qest', 'np', 'optparams', 'covmodel','lambda','alpha','likelihood','-v7.3')
+     disp(toc)
 end
 
 end
--- fullScaleBGLTaperedMatern.m
+++ fullScaleBGLTaperedMatern.m
@@ -1,40 +1,46 @@
-function fullScaleBGLMOW(input)
+%% Full scale basis graphical lasso
+function fullScaleBGLTaperedMatern(lambdas,lvls)
+% clear variables; close all;
+%% Full scale basis graphical lasso
+% clear variables; close all;
 fprintf('Running Full-Scale Basis Graphical Lasso with %d levels of resolution and lambdas %.4f to %.4f\n'...
-    ,input.lvls,input.lambda(1),input.lambda(end))
-fprintf('*******MMIXTURE OF WENDLANDS********\n');
-%% Parameters for determining residual model parameters
-% Not needed for the BGL
+    ,lvls,lambdas(1),lambdas(end))
+% nneeds = [12,60,252,1020]+1;
+fprintf('*******TAPERED MATERN********\n');
 optparams = struct; 
 optparams.algorithm='simanneal';
 optparams.ranges = [[-3,3];%log10(tau^2)
-                    [1, 20];%theta1
-                    [0.01,600];%alpha1
-                    [1,20];%theta2
-                    [0.01,600]]; %alpha2
+                    [0.01,2000];%alpha
+                       [0.1,40];%Taper range
+                       [0.1,1];%Smoothness
+                       [0.1,40]];%Matern range
+% optparams.ranges = [-10,1];
 optparams.maxiters = 5000; %number of simulated annealing steps
 optparams.npts = 501; %Number of lattice points
-optparams.x0 = [0, 10, 300, 10, 300];%Initial guess  7.65, 301.205,
+optparams.x0 = [0, 1000, 20, 0.5, 20];%Initial guess
 optparams.pchange = 0.9;
-%% Distance measures
+
+
 fprintf('********************Fitting residual model**********************\n');
-[spx,spy,spz] = sph2cart(pi/180*input.LON(:), pi/180*input.LAT(:),1);
+
+[spx,spy,spz] = sph2cart(pi/180*LON(:), pi/180*LAT(:),1);
 spc=[spx,spy,spz];
 d = real(acosd(spc*spc')) ; %d = d - diag(diag(d));
 %imaginary values on the diagonal sometimes        
-covmodel = @(x)MixOfWendlandCov(x,d,'cov');      
-%% 
-fnname = func2str(covmodel);
+covmodel = @(x)TaperedMatern( d,x);
+fnname = func2str(covmodel{1});
 fnname = fnname(5:end);
-ndx = strfind(fnname, '(');
-fnname = fnname(1:ndx-1);
-% separation = rand([1,64800]);
-% ndcs = separation < 0.25;dtmp = d(ndcs,ndcs);
+ndx = strfind(fnname, '('); 
+fnname = fnname(1:ndx(1)-1);
 [val,obj,np] = CalculateInitialGuess(covmodel,input.Psi,input.data, optparams);
+% np = [-1.108,-0.816,1032,27.55,0.6346,7.5214];
 fprintf('Done\n');
+
 for lambda = input.lambda       
     alpha = np(1);
     [Qest] = CalculateQ(input.data, input.Psi, lambda, input.tol,covmodel,np(2:end), 1 );
     save(fullfile(input.svDir,sprintf('FSBGL results %d levels lambda %.4f covmodel %s.mat',input.lvls,lambda , fnname)),'alpha', 'Qest', 'np', 'optparams', 'covmodel','lambda','-v7.3')
 end
 end
+ 
 
